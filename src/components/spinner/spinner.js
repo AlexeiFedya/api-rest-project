1f52344669a4b1ec6c60d222148ceec5c042407b
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 18:22:20 +0300

    star wars api service

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return(
+        <div className="loadingio-spinner-ripple-sflsryd80ej"><div className="ldio-jh1rprtdfs">
+            <div></div><div></div>
+            </div></div>
+    )
+}
+
+export default Spinner;
