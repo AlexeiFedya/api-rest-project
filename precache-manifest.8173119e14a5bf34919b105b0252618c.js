21454bbb170d856d829e4347c81a8ea856d2726e
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 18:47:08 +0300

    Updates

---
+++ precache-manifest.8173119e14a5bf34919b105b0252618c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678026267b7c84bd02dae04a1d7e6a84",
+    "url": "/api-rest-project/index.html"
+  },
+  {
+    "revision": "de2252078f957d88b622",
+    "url": "/api-rest-project/static/css/main.059cadec.chunk.css"
+  },
+  {
+    "revision": "88a67c75ae03ba19e125",
+    "url": "/api-rest-project/static/js/2.d0461379.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/api-rest-project/static/js/2.d0461379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2252078f957d88b622",
+    "url": "/api-rest-project/static/js/main.e7f9929e.chunk.js"
+  },
+  {
+    "revision": "451d8b212b431315ed18",
+    "url": "/api-rest-project/static/js/runtime-main.64ad79da.js"
+  }
+]);
